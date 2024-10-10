5c2bbb2ce308e7f0fe023f1df6459526ed9e4b1e
zlatapolikova <poliakova.zlata@student.uzhnu.edu.ua>
2024-10-10 12:06:01 +0300

    add prettier, gitignor and eslint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "./App.css";
 
 const App = () => {
   return (
